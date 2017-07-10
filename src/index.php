0adf3ccb17494f8f84e4e51c001516d70488c639
vasiliytsykin <vasiliytsykin@gmail.com>
2017-07-10 17:56:08 +0500

    small fixes

--- src/index.php
+++ src/index.php
@@ -1,18 +1,5 @@
 <?include $_SERVER["DOCUMENT_ROOT"].'/tmp/header.php'?>
 
-<div class="page">
-	<div class="gallery">
-		<div class="gallery__switch">
-			<div class="switch__tabs">
-				<div class="switch__tab" data-tab="tab-1">tab 1</div>
-				<div class="switch__tab" data-tab="tab-2">tab 2</div>
-			</div>
-		</div>
-		<div class="gallery__tabs">
-			<div class="gallery__tab tab-1"></div>
-			<div class="gallery__tab tab-2"></div>
-		</div>
-	</div>
-</div>
+
 
 <?include $_SERVER["DOCUMENT_ROOT"].'/tmp/footer.php'?>
